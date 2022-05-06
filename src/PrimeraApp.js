7b98edac00e4a38041661bec85288587d7a09a4b
Herm Franco <herman.franco@devlabroom.com>
2022-05-05 22:06:05 -0500

    Primeras practicas

---
+++ src/PrimeraApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PrimeraApp = () => {
+
+    const saludo = 'Hola Mundo';
+
+   return  (
+        <>
+        <h1> { saludo } </h1>
+        <p>Mi primera App</p>
+
+        </>
+)   
+}
+
+export default  PrimeraApp;
